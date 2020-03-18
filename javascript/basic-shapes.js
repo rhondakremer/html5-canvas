3f8bc30691ca525fc26adf228c567785592f803e
Rhonda Kremer <rhondakremer@gmail.com>
2020-03-18 18:37:13 -0400

    iframe for displaying different canvas options

---
+++ javascript/basic-shapes.js
@@ -0,0 +1,22 @@
+// Rectangle
+// c.fillStyle = "rgba(255, 0, 0, 0.5)"
+// c.fillRect(100, 100, 100, 100); // c.fillRect(x, y, width, height)
+// c.fillStyle = "rgba(0, 0, 255, 0.5)"
+// c.fillRect(400, 200, 100, 100);
+// c.fillStyle = "rgba(0, 255, 0, 0.5)"
+// c.fillRect(500, 350, 100, 100);
+// console.log(canvas);
+
+// Line
+// c.beginPath();
+// c.moveTo(50, 300); // c.moveTo(x, y)
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3"
+// c.stroke();
+
+// Arc / Circle 
+// c.beginPath(); // include or else it will connect to the stroke line from before
+// c.arc(300, 300, 30, 0, Math.PI * 2, false) // (x, y, radius, startAngle, endAngle, counterclockwise)
+// c.strokeStyle = "blue"
+// c.stroke()
--- javascript/bubbles.js
+++ javascript/bubbles.js
@@ -6,39 +6,6 @@ canvas.height = window.innerHeight;
 // context variable- creating a context object
 let c = canvas.getContext('2d')
 
-// Rectangle
-// c.fillStyle = "rgba(255, 0, 0, 0.5)"
-// c.fillRect(100, 100, 100, 100); // c.fillRect(x, y, width, height)
-// c.fillStyle = "rgba(0, 0, 255, 0.5)"
-// c.fillRect(400, 200, 100, 100);
-// c.fillStyle = "rgba(0, 255, 0, 0.5)"
-// c.fillRect(500, 350, 100, 100);
-// console.log(canvas);
-
-// Line
-// c.beginPath();
-// c.moveTo(50, 300); // c.moveTo(x, y)
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = "#fa34a3"
-// c.stroke();
-
-// Arc / Circle 
-// c.beginPath(); // include or else it will connect to the stroke line from before
-// c.arc(300, 300, 30, 0, Math.PI * 2, false) // (x, y, radius, startAngle, endAngle, counterclockwise)
-// c.strokeStyle = "blue"
-// c.stroke()
-
-// create a bunch of circles at once with a for loop mf
-// for (let i = 0; i < 300; i++) {
-//     let x = Math.random() * window.innerWidth;
-//     let y = Math.random() * window.innerHeight;
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, Math.PI * 2, false);
-//     c.strokeStyle = "orange";
-//     c.stroke();
-// }
-
 // Animation
 // initialize mouse position object
 var mouse = {
